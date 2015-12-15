ce72dbab3d8067e7825d0ee2230069811030e7c2
carmarolli <christophe.armarolli@4sh.fr>
2015-12-15 21:49:14 +0100

    Add ending circle functionality;

--- js/jquery.circliful.js
+++ js/jquery.circliful.js
@@ -1,80 +1,101 @@
-"use strict";
-
 (function ($) {
 
-    $.fn.circliful = function (options, callback) {
+    $.fn.circliful = function (options) {
 
         var settings = $.extend({
             // These are the defaults.
-            startdegree: 0,
-            fgcolor: "#556b2f",
-            bgcolor: "#eee",
-            fill: false,
+            foregroundColor: "#556b2f",
+            backgroundColor: "#eee",
+            fillColor: false,
             width: 15,
             dimension: 200,
-            fontsize: 15,
+            size: 15,
             percent: 50,
-            animationstep: 1.0,
-            iconsize: '20px',
-            iconcolor: '#999',
-            border: 'default',
-            complete: null,
-            bordersize: 10
+            animationStep: 1.0
         }, options);
-
         return this.each(function () {
-
-            var customSettings = ["fgcolor", "bgcolor", "fill", "width", "dimension", "fontsize", "animationstep", "endPercent", "icon", "iconcolor", "iconsize", "border", "startdegree", "bordersize"];
-
-            var customSettingsObj = {};
+            var dimension = '';
+            var text = '';
+            var info = '';
+            var width = '';
+            var size = 0;
+            var percent = 0;
+            var endPercent = 100;
+            var fgcolor = '';
+            var bgcolor = '';
             var icon = '';
-            var percent;
-            var endPercent = 0;
-            var obj = $(this);
-            var fill = false;
-            var text, info;
+            var animationstep = 0.0;
 
-            obj.addClass('circliful');
+            $(this).addClass('circliful');
 
-            checkDataAttributes(obj);
+            if ($(this).data('dimension') != undefined) {
+                dimension = $(this).data('dimension');
+            } else {
+                dimension = settings.dimension;
+            }
 
-            if (obj.data('text') != undefined) {
-                text = obj.data('text');
+            if ($(this).data('width') != undefined) {
+                width = $(this).data('width');
+            } else {
+                width = settings.width;
+            }
 
-                if (obj.data('icon') != undefined) {
-                    icon = $('<i></i>')
-                        .addClass('fa ' + $(this).data('icon'))
-                        .css({
-                            'color': customSettingsObj.iconcolor,
-                            'font-size': customSettingsObj.iconsize
-                        });
+            if ($(this).data('fontsize') != undefined) {
+                size = $(this).data('fontsize');
+            } else {
+                size = settings.size;
+            }
+
+            if ($(this).data('percent') != undefined) {
+                percent = $(this).data('percent') / 100;
+                endPercent = $(this).data('percent');
+            } else {
+                percent = settings.percent / 100;
+            }
+
+            if ($(this).data('fgcolor') != undefined) {
+                fgcolor = $(this).data('fgcolor');
+            } else {
+                fgcolor = settings.foregroundColor;
+            }
+
+            if ($(this).data('bgcolor') != undefined) {
+                bgcolor = $(this).data('bgcolor');
+            } else {
+                bgcolor = settings.backgroundColor;
+            }
+
+            if ($(this).data('animation-step') != undefined) {
+                animationstep = parseFloat($(this).data('animation-step'));
+            } else {
+                animationstep = settings.animationStep;
+            }
+            if ($(this).data('text') != undefined) {
+                text = $(this).data('text');
+
+                if ($(this).data('icon') != undefined) {
+                    icon = '<i class="fa ' + $(this).data('icon') + '"></i>';
                 }
 
-                if (obj.data('type') != undefined) {
+                if ($(this).data('type') != undefined) {
                     type = $(this).data('type');
 
                     if (type == 'half') {
-                        addCircleText(obj, 'circle-text-half', (customSettingsObj.dimension / 1.45));
+                        $(this).append('<span class="circle-text-half">' + icon + text + '</span>');
+                        $(this).find('.circle-text-half').css({
+                            'line-height': (dimension / 1.45) + 'px',
+                            'font-size': size + 'px'
+                        });
                     } else {
-                        addCircleText(obj, 'circle-text', customSettingsObj.dimension);
+                        $(this).append('<span class="circle-text">' + icon + text + '</span>');
+                        $(this).find('.circle-text').css({'line-height': dimension + 'px', 'font-size': size + 'px'});
                     }
                 } else {
-                    addCircleText(obj, 'circle-text', customSettingsObj.dimension);
+                    $(this).append('<span class="circle-text">' + icon + text + '</span>');
+                    $(this).find('.circle-text').css({'line-height': dimension + 'px', 'font-size': size + 'px'});
                 }
-            }
+            } else if ($(this).data('icon') != undefined) {
 
-            if ($(this).data("total") != undefined && $(this).data("part") != undefined) {
-                var total = $(this).data("total") / 100;
-
-                percent = (($(this).data("part") / total) / 100).toFixed(3);
-                endPercent = ($(this).data("part") / total).toFixed(3);
-            } else {
-                if ($(this).data("percent") != undefined) {
-                    percent = $(this).data("percent") / 100;
-                    endPercent = $(this).data("percent");
-                } else {
-                    percent = settings.percent / 100;
-                }
             }
 
             if ($(this).data('info') != undefined) {
@@ -84,179 +105,106 @@
                     type = $(this).data('type');
 
                     if (type == 'half') {
-                        addInfoText(obj, 0.9);
+                        $(this).append('<span class="circle-info-half">' + info + '</span>');
+                        $(this).find('.circle-info-half').css({'line-height': (dimension * 0.9) + 'px',});
                     } else {
-                        addInfoText(obj, 1.25);
+                        $(this).append('<span class="circle-info">' + info + '</span>');
+                        $(this).find('.circle-info').css({'line-height': (dimension * 1.25) + 'px',});
                     }
                 } else {
-                    addInfoText(obj, 1.25);
+                    $(this).append('<span class="circle-info">' + info + '</span>');
+                    $(this).find('.circle-info').css({'line-height': (dimension * 1.25) + 'px',});
                 }
             }
 
-            $(this).width(customSettingsObj.dimension + 'px');
+            $(this).width(dimension + 'px');
 
-            var size = customSettingsObj.dimension,
-                canvas = $('<canvas></canvas>').attr({
-                    width: size,
-                    height: size
-                }).appendTo($(this)).get(0);
-
+            var canvas = $('<canvas></canvas>').attr({width: dimension, height: dimension}).appendTo($(this)).get(0);
             var context = canvas.getContext('2d');
-
-            var dpr = window.devicePixelRatio;
-            if ( dpr ) {
-                var $canvas = $(canvas);
-                $canvas.css('width', size);
-                $canvas.css('height', size);
-                $canvas.attr('width', size * dpr);
-                $canvas.attr('height', size * dpr);
-
-                context.scale(dpr, dpr);
-            }
-
-            var container = $(canvas).parent();
-            var x = size / 2;
-            var y = size / 2;
-            var degrees = customSettingsObj.percent * 360.0;
+            var x = canvas.width / 2;
+            var y = canvas.height / 2;
+            var degrees = percent * 360.0;
             var radians = degrees * (Math.PI / 180);
-            var radius = size / 2.5;
+            var radius = canvas.width / 2.5;
             var startAngle = 2.3 * Math.PI;
             var endAngle = 0;
             var counterClockwise = false;
-            var curPerc = customSettingsObj.animationstep === 0.0 ? endPercent : 0.0;
-            var curStep = Math.max(customSettingsObj.animationstep, 0.0);
+            var curPerc = animationstep === 0.0 ? endPercent : 0.0;
+            var curStep = Math.max(animationstep, 0.0);
             var circ = Math.PI * 2;
             var quart = Math.PI / 2;
             var type = '';
-            var fireCallback = true;
-            var additionalAngelPI = (customSettingsObj.startdegree / 180) * Math.PI;
+            var fill = false;
 
             if ($(this).data('type') != undefined) {
                 type = $(this).data('type');
 
                 if (type == 'half') {
-                    startAngle = 2.0 * Math.PI;
-                    endAngle = 3.13;
-                    circ = Math.PI;
-                    quart = Math.PI / 0.996;
-                }
-            }
-            
-            if ($(this).data('type') != undefined) {
-                type = $(this).data('type');
-
-                if (type == 'angle') {
-                    startAngle = 2.25 * Math.PI;
-                    endAngle = 2.4;
-                    circ = 1.53 + Math.PI;
-                    quart = 0.73 + Math.PI / 0.996;
+                    var startAngle = 2.0 * Math.PI;
+                    var endAngle = 3.13;
+                    var circ = Math.PI * 1.0;
+                    var quart = Math.PI / 0.996;
                 }
             }
 
-            /**
-             * adds text to circle
-             *
-             * @param obj
-             * @param cssClass
-             * @param lineHeight
-             */
-            function addCircleText(obj, cssClass, lineHeight) {
-                $("<span></span>")
-                    .appendTo(obj)
-                    .addClass(cssClass)
-                    .html(text)
-                    .prepend(icon)
-                    .css({
-                        'line-height': lineHeight + 'px',
-                        'font-size': customSettingsObj.fontsize + 'px'
-                    });
+            if ($(this).data('fill') != undefined) {
+                fill = $(this).data('fill');
+            } else {
+                fill = settings.fillColor;
             }
 
-            /**
-             * adds info text to circle
-             *
-             * @param obj
-             * @param factor
-             */
-            function addInfoText(obj, factor) {
-                $('<span></span>')
-                    .appendTo(obj)
-                    .addClass('circle-info-half')
-                    .css(
-                        'line-height', (customSettingsObj.dimension * factor) + 'px'
-                    )
-                    .text(info);
+            function drawEndingCircle(x, y, settings) {
+                context.beginPath();
+                context.arc(x, y, settings.width, 0, 2 * Math.PI);
+                context.strokeStyle = settings.strokeColor;
+                context.fillStyle = settings.fillColor;
+                context.fill();
+                context.stroke();
             }
 
-            /**
-             * checks which data attributes are defined
-             * @param obj
-             */
-            function checkDataAttributes(obj) {
-                $.each(customSettings, function (index, attribute) {
-                    if (obj.data(attribute) != undefined) {
-                        customSettingsObj[attribute] = obj.data(attribute);
-                    } else {
-                        customSettingsObj[attribute] = $(settings).attr(attribute);
-                    }
-
-                    if (attribute == 'fill' && obj.data('fill') != undefined) {
-                        fill = true;
-                    }
-                });
-            }
-
-            /**
-             * animate foreground circle
-             * @param current
-             */
+            //animate foreground circle
             function animate(current) {
-
                 context.clearRect(0, 0, canvas.width, canvas.height);
 
                 context.beginPath();
                 context.arc(x, y, radius, endAngle, startAngle, false);
+                context.lineWidth = width - 1;
 
-                context.lineWidth = customSettingsObj.bordersize + 1;
-
-                context.strokeStyle = customSettingsObj.bgcolor;
+                // line color
+                context.strokeStyle = bgcolor;
                 context.stroke();
 
-                if (customSettingsObj.fill) {
-                    context.fillStyle = customSettingsObj.fill;
+                if (fill) {
+                    context.fillStyle = fill;
                     context.fill();
                 }
 
                 context.beginPath();
-                context.arc(x, y, radius, -(quart) + additionalAngelPI, ((circ) * current) - quart + additionalAngelPI, false);
-
-                if (customSettingsObj.border == 'outline') {
-                    context.lineWidth = customSettingsObj.width + customSettingsObj.bordersize;
-                } else if (customSettingsObj.border == 'inline') {
-                    context.lineWidth = customSettingsObj.width - customSettingsObj.bordersize;
-                }
-
-                context.strokeStyle = customSettingsObj.fgcolor;
+                context.arc(x, y, radius, -(quart), ((circ) * current) - quart, false);
+                context.lineWidth = width;
+                // line color
+                context.strokeStyle = fgcolor;
                 context.stroke();
+
+                // Draw little circle on the arc
+                var xpos = x + Math.cos(((circ) * current) - quart) * radius;
+                var ypos = y + Math.sin(((circ) * current) - quart) * radius;
+                if (settings.endingCircle) {
+                    drawEndingCircle(xpos, ypos, settings.endingCircle);
+                }
 
                 if (curPerc < endPercent) {
                     curPerc += curStep;
                     requestAnimationFrame(function () {
                         animate(Math.min(curPerc, endPercent) / 100);
-                    }, obj);
+                    });
                 }
 
-                if (curPerc == endPercent && fireCallback && typeof(options) != "undefined") {
-                    if ($.isFunction(options.complete)) {
-                        options.complete();
-
-                        fireCallback = false;
-                    }
-                }
             }
-
             animate(curPerc / 100);
 
         });
+
     };
+
 }(jQuery));
